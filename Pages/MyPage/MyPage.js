13402988784d8ed655fc9ec20fec7fae2618379e
pshhhhhhhhhh <winki93@naver.com>
2022-11-23 00:37:58 +0900

    메인에서 뻗어 나옴 기존 브랜치 삭제
    여기서 연지씨랑 희정씨거 합칠것

---
+++ Pages/MyPage/MyPage.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { Text, View , SafeAreaView, StyleSheet, TextInput , Button, Alert } from 'react-native';
+import Constants from 'expo-constants';
+import BottomTab from '../../Components/BottomTab';
+// import { Text, View, Button } from "react-native";
+
+
+
+export default function MyPage(navigation){
+    return(
+    <View>
+      <Text>MyPage페이지입니다.</Text>
+    </View>
+    )
+}
+
+
