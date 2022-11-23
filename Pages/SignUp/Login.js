1e5b564a3af90345762fe6adc9d1d96585f510fe
pshhhhhhhhhh <winki93@naver.com>
2022-11-23 11:54:42 +0900

    프론트 합침
    제대로 합침

--- Pages/SignUp/Login.js
+++ Pages/SignUp/Login.js
@@ -1,8 +1,6 @@
 import 'react-native-gesture-handler';
-import React from "react";
 //페이지 import
 import Join from './Join';
-import { Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 // import { createStackNavigator } from "react-navigation-stack";
 import { createStackNavigator } from "@react-navigation/stack";
