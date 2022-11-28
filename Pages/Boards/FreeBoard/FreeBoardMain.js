a5ed42e644186dd115b5e77414c608ef906aa8c4
pshhhhhhhhhh <winki93@naver.com>
2022-11-29 03:47:17 +0900

    자유게시판 연동완료
    완벽하지 않음

--- Pages/Boards/FreeBoard/FreeBoardMain.js
+++ Pages/Boards/FreeBoard/FreeBoardMain.js
@@ -1,41 +1,113 @@
 import axios from 'axios';
-import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { Text, View, ScrollView, SafeAreaView, StyleSheet, TextInput, Button, Alert } from 'react-native';
-import Constants from 'expo-constants';
+import { Text, View, ScrollView, SafeAreaView, Image, StyleSheet, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
 import FreeView from '../../../Components/FreeView';
 
 
 
 export default function FreeBoardMain({ navigation }) {
 
-  const pressHandler = () => {
-    // 페이지 이동. navigate = push
-    // 변수는 routes 파일 screens 내 키값을 사용한다
-    navigation.navigate('FreeBoardDetail');
-    // navigation.push('ReviewDetail');
+
+
+
+
+
+
+
+  const [frData, setFrData] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.post("http://192.168.2.94:5000/board/search", null, {
+      params: {
+        page: 1,
+        size: 10,
+        type: "type",
+        keyword: "자유게시판"
+      }
+    })
+      .then(function (res) {
+        setFrData(res.data.dtoList);
+        console.log("리스폰스데이터:", res.data.dtoList[0].bno);
+      })
+      .catch(function (error) {
+        console.log("게시판 전체 데이터 가져오기 실패: ", error)
+      })
+  }, [])
+
+  const onDetail = () => {
+    navigation.navigate("FreeBoardDetail", { no: frData.bno });
   }
 
+
   return (
-    <View style={{ width: "100%", height: "100%"}}>
-      
-      <FreeView/>
-      <View>
-      <Button title="자유게시판 디테일로 이동"
-          onPress={pressHandler}
-        />
-      </View>
-    </View>
-  )
+    
+    <ScrollView> 
+    {frData.map((e) =>(
+      <FreeView key={e.id} {...e}/>
+        )   
+      )
+    }
+    </ScrollView>
+
+  );
 }
+
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     width: "100%",
-    height: "100%",
-    borderWidth:1
-
+    height: "50%",
+    marginBottom: 20,
+    padding: 5
+  },
+  imageView: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "50%",
+    borderWidth: 3,
+    borderBottomWidth: 0,
+    height: "40%",
+    borderColor: "black"
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  contentBox: {
+    alignItems: "center",
+    width: "50%",
+    height: "50%",
+    borderWidth: 2,
+    borderTopWidth: 0,
+    borderColor: "black",
+    backgroundColor: "#F1E7DD",
+    padding: 10
+  },
+  title: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems: "center",
+    width: "100%",
+    height: "50%",
+    backgroundColor: "#F1E7DD"
+  },
+  titleText: {
+    fontWeight: "bold",
+    fontSize: 10
+  },
+  content: {
+    width: "100%",
+    height: "50%",
+    backgroundColor: "#F1E7DD"
+  },
+  bottomContent: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems: "center",
+    width: "100%",
+    height: "50%",
+    backgroundColor: "#F1E7DD"
   },
 
 });
-
--- Pages/Main/Main.js
+++ Pages/Main/Main.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { Text, View , SafeAreaView, StyleSheet, TextInput , Button, Alert,ScrollView,TouchableOpacity  } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet, TextInput, Button, Alert, ScrollView, TouchableOpacity } from 'react-native';
 import { useEffect } from 'react';
 import { BackHandler } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -11,7 +11,7 @@ import { useRoute } from '@react-navigation/native';
 import { useFocusEffect } from '@react-navigation/native';
 
 
-export default function Main({ navigation}){
+export default function Main({ navigation }) {
 
   useFocusEffect(
     React.useCallback(() => {
@@ -25,91 +25,84 @@ export default function Main({ navigation}){
         ]);
         return true;
       };
-  
+
       const backHandler = BackHandler.addEventListener(
         "hardwareBackPress",
         backAction
       );
-  
+
       return () => backHandler.remove();
 
     }, []))
 
-        
-    return(
-    <View style={{width:"100%",height:"100%",borderWidth:1}}>
-      <View style={{width:"100%",height:"18%",flexDirection:"row",justifyContent:"center",marginBottom:5}}>
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,alignItems:"center",justifyContent:"center",margin:7}}
-        onPress={() => navigation.navigate('MainBoard')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>게시판들!</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>Board</Text>
-        </TouchableOpacity>   
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,borderColor:"white",alignItems:"center",justifyContent:"center",marginTop:7,marginBottom:7}}
-        onPress={() => navigation.navigate('CalenderMain')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>캘린더</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>Calender</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,alignItems:"center",justifyContent:"center",margin:7}}>
-        <Text style={{fontWeight:"bold",fontSize:20}}>함께하는공간</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>Map</Text>
-        </TouchableOpacity> 
-      </View>
-      <View style={{width:"100%",height:"18%",flexDirection:"row",justifyContent:"center",marginBottom:5}}>
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,borderColor:"white",alignItems:"center",justifyContent:"center",margin:7}}
-        onPress={() => navigation.navigate('Walk')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>산책해요</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>Walk</Text>
-        </TouchableOpacity> 
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,alignItems:"center",justifyContent:"center",marginTop:7}}
-        onPress={() => navigation.navigate('News')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>애완뉴스</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>News</Text>
-        </TouchableOpacity> 
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,alignItems:"center",justifyContent:"center",margin:7}}
-        onPress={() => navigation.navigate('Ranking')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>최고의 견주</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>Ranking</Text>
-        </TouchableOpacity> 
-      </View>
-      <View style={{width:"100%",height:"18%",flexDirection:"row",justifyContent:"flex-start",marginBottom:5,marginLeft:5}}>
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,borderColor:"white",alignItems:"center",justifyContent:"center",margin:7}}
-        onPress={() => navigation.navigate('SkinMain')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>피부검사</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>Skin</Text>
-        </TouchableOpacity> 
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,alignItems:"center",justifyContent:"center",marginTop:7}}
-        onPress={() => navigation.navigate('MyPage')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>내정보</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>MyPage</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,alignItems:"center",justifyContent:"center",margin:7}}
-        onPress={() => navigation.navigate('Play')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>놀아주세요</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>Play</Text>
-        </TouchableOpacity>          
-      </View>
-      <View style={{width:"100%",height:"18%",flexDirection:"row",justifyContent:"flex-start",marginBottom:5,marginLeft:5}}>
-        <TouchableOpacity style={{width:"30%",height:"100%",borderWidth:2.5,alignItems:"center",justifyContent:"center",margin:7}}
-        onPress={() => navigation.navigate('Food')}
-        >
-        <Text style={{fontWeight:"bold",fontSize:20}}>밥챙겨주기</Text>
-        <Text style={{fontWeight:"bold",fontSize:20}}>Food</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={{width:"100%",height:"100%",marginTop:10,flexDirection:"row"}}>
-      <FreeView/>
-      <FreeView/>
-      </View>
-    </View>
-    )
 
-    
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={{ width: "100%", height: "100%", borderWidth: 1 }}>
+          <View style={{ width: "100%", height: "15%", flexDirection: "row", justifyContent: "center", marginBottom: 5 }}>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, alignItems: "center", justifyContent: "center", margin: 7 }}
+              onPress={() => navigation.navigate('MainBoard')}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>게시판들!</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>Board</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, borderColor: "white", alignItems: "center", justifyContent: "center", marginTop: 7, marginBottom: 7 }}
+              onPress={() => navigation.navigate('CalenderMain')}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>캘린더</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>Calender</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, alignItems: "center", justifyContent: "center", margin: 7 }}>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>함께하는공간</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>Map</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ width: "100%", height: "15%", flexDirection: "row", justifyContent: "center", marginBottom: 5 }}>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, borderColor: "white", alignItems: "center", justifyContent: "center", margin: 7 }}
+              onPress={() => navigation.navigate('Walk')}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>산책해요</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>Walk</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, alignItems: "center", justifyContent: "center", marginTop: 7 }}
+              onPress={() => navigation.navigate('News')}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>애완뉴스</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>News</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, alignItems: "center", justifyContent: "center", margin: 7 }}
+              onPress={() => navigation.navigate('Ranking')}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>최고의 견주</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>Ranking</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ width: "100%", height: "15%", flexDirection: "row", justifyContent: "center", marginBottom: 5 }}>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, borderColor: "white", alignItems: "center", justifyContent: "center", margin: 7 }}
+              onPress={() => navigation.navigate('SkinMain')}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>피부검사</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>Skin</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, alignItems: "center", justifyContent: "center", marginTop: 7 }}
+              onPress={() => navigation.navigate('Food')}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>밥챙겨주기</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>Food</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: "30%", height: "100%", borderWidth: 2.5, alignItems: "center", justifyContent: "center", margin: 7 }}
+              onPress={() => navigation.navigate('Play')}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>놀아주세요</Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>Play</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ width: "50%", height: "100%", marginTop: 10, flexDirection: "row" }}>
+          
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
